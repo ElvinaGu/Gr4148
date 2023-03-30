26f915020a5c782e6e2f49effc242fa8acb8c022
Elvina <Elka121194@yandex.ru>
2023-03-30 22:15:50 +0300

    ReadData & LineGen

---
+++ Sem3Task22/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача №22
+// Напишите программу, которая принимает на вход число (N) и 
+// выдаёт таблицу квадратов чисел от 1 до N.
+
+Console.Clear();
+//Метод читает данные от пользователя
+int ReadData(string msg)
+{
+  Console.WriteLine(msg);
+  int res = int.Parse(Console.ReadLine() ?? "0");
+  return res;
+}
+
+//Метод генерации
+string LineGen(int num, int pow)
+{
+  string result = String.Empty;
+
+  for (int i = 1; i <= num; i++)
+  {
+    result = result + Math.Pow(i, pow) + "_";
+  }
+  return result;
+}
+
+int numN = ReadData("Введите число N: ");
+Console.WriteLine(LineGen(numN,1));
+Console.WriteLine(LineGen(numN,2));
