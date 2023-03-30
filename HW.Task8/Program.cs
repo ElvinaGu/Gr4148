83f4bf5a45286873384f8afad5aa9823cacbcb2d
Elvina <Elka121194@yandex.ru>
2023-03-30 16:49:59 +0300

    even numbers from 1 to N

---
+++ HW.Task8/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход принимает число (N),
+// а на выходе показывает все чётные числа от 1 до N
+
+Console.Write("Напишите число ");
+string? number = Console.ReadLine();
+
+if (number != null)
+{
+  int Num1 = int.Parse(number);
+
+  for (int i = 2; i <= Num1; i += 2)
+  {
+    Console.WriteLine(i);
+  }
+}
