60c449f6d8e466aa429baa78d02fb0fa92a8e3ed
Elvina <Elka121194@yandex.ru>
2023-03-30 22:15:18 +0300

    ReadData & CalcLen2D

---
+++ Sem3Task20/Program.cs
@@ -0,0 +1,22 @@
+﻿//Задача №20
+// Напишите программу, которая принимает на вход координаты двух точек
+// и находит расстояние между ними в 2D пространстве.
+
+//Метод читает данные от пользователя
+int ReadData(string msg)
+{
+  Console.WriteLine(msg);
+  int res = int.Parse(Console.ReadLine() ?? "0");
+  return res;
+}
+double CalcLen2D(int x1, int y1, int x2, int y2)
+{
+  return Math.Sqrt(Math.Pow((x1 - x2), 2) + (y1 - y2) * (y1 - y2));
+}
+// Запрос координат
+int x1 = ReadData("Введите координату x точки A: ");
+int y1 = ReadData("Введите координату y точки A: ");
+int x2 = ReadData("Введите координату x точки B: ");
+int y2 = ReadData("Введите координату y точки B: ");
+
+Console.WriteLine("Расстояние между точками A и B: " + CalcLen2D(x1, y1, x2, y2));
