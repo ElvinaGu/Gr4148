5684f33d64066e65ef7d0232d113cac187c6148f
Elvina <Elka121194@yandex.ru>
2023-03-30 16:45:40 +0300

    find max and min

---
+++ HW.Task2/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, 
+// какое число большее, а какое меньшее.
+
+Console.Write("Напишите число ");
+string? a1 = Console.ReadLine();
+Console.Write("Напишите число ");
+string? b1 = Console.ReadLine();
+
+if (a1 != null && b1 != null)
+{
+  int a = int.Parse(a1);
+  int b = int.Parse(b1);
+
+  if (a > b)
+  {
+    Console.Write("Наибольшим числом является ");
+    Console.WriteLine(a);
+    Console.Write("Наименьшим числом является ");
+    Console.WriteLine(b);
+  }
+  else
+  {
+    if (a == b)
+    {
+      Console.WriteLine("Числа равны");
+    }
+    else
+    {
+      Console.Write("Наибольшим числом является ");
+      Console.WriteLine(b);
+      Console.Write("Наименьшим числом является ");
+      Console.WriteLine(a);
+    }
+  }
+}
