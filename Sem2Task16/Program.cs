b0fa0b312261de778a6cb545779154fe36cb0315
Elvina <Elka121194@yandex.ru>
2023-04-03 14:28:22 +0300

    A number is the square of another number

---
+++ Sem2Task16/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача №16
+// Напишите программу, которая принимает на вход два числа и проверяет, 
+// является ли одно число квадратом другого.
+
+int num1 = int.Parse(Console.ReadLine() ?? "0");
+int num2 = int.Parse(Console.ReadLine() ?? "0");
+
+void SqrtTest(int a, int b)
+{
+  if (a == b * b)
+    Console.WriteLine(a + " квадрат числа " + b);
+  else
+    Console.WriteLine(a + " не квадрат числа " + b);
+}
+SqrtTest(num1, num2);
+SqrtTest(num2, num1);
