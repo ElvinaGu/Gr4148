913a1bdeb521e563e264f8b18860cf6d0cc0cae8
Elvina <Elka121194@yandex.ru>
2023-04-03 23:17:07 +0300

    Count Digit

---
+++ Sem4Task26/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача №26
+// Напишите программу, которая принимает на вход
+// число и выдаёт количество цифр в числе.
+
+//Метод читает данные от пользователя
+int ReadData(string msg)
+{
+  Console.WriteLine(msg);
+  int res = int.Parse(Console.ReadLine() ?? "0");
+  return res;
+}
+
+//Метод вывода результата
+void PrintResult(string msg, long res)
+{
+  Console.WriteLine(msg + res);
+}
+
+//Метод подсчета цифр
+long CountDigit(long num)
+{
+  int res = 0;
+  //res=(long)Math.Log(num)+1;
+  while (num > 0)
+  {
+    res = res + 1;
+    num = num / 10;
+  }
+  return res;
+}
+
+long numberA = ReadData("Введите число: ");
+long res1 = CountDigit(numberA);
+PrintResult("Количеств цифр: ", (long)res1);
