b1cb12dede09b2401d40f52e50abd6c377c7fcfb
Elvina <Elka121194@yandex.ru>
2023-04-03 15:31:45 +0300

    The third digit of the number

---
+++ HW.S2.Task13/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа
+// или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine() ?? "0");
+char[] arr = num.ToString().ToCharArray();
+
+if (arr.Length >= 4)
+{
+  Console.WriteLine(arr[2]);
+}
+else
+{
+  Console.WriteLine("Такой цифры нет");
+}
