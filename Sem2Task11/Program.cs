fcf43388308dba862d430976a9c26a4d732ea1b0
Elvina <Elka121194@yandex.ru>
2023-04-03 14:22:41 +0300

    Random three-digit number and delete second digit

---
+++ Sem2Task11/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача №11
+// Напишите программу, которая выводит случайное трёхзначное число и удаляет 
+// вторую цифру этого числа.
+// 123 -> 13
+
+int num = new Random().Next(100, 1000);
+Console.WriteLine(num);
+int firstNum = num / 100;
+int LastNum = num % 10;
+int newNum = firstNum * 10 + LastNum;
+Console.WriteLine(newNum);
