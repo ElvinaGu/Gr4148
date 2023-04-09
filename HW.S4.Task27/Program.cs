d9aaace5e0f952e4fa166ecb76d004904cea42d5
Elvina <Elka121194@yandex.ru>
2023-04-09 23:20:10 +0300

    Digit sum in number

---
+++ HW.S4.Task27/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 27: 
+// Напишите программу, которая принимает на вход число 
+// и выдаёт сумму цифр в числе.
+
+//Метод читает данные от пользователя
+int ReadData(string msg)
+{
+  Console.Write(msg);
+  int res = int.Parse(Console.ReadLine() ?? "0");
+  return res;
+}
+
+//Метод вывода результата
+void PrintResult(string msg, int res)
+{
+  Console.WriteLine(msg + res);
+}
+
+//Метод суммирования цифр в числе
+int digitSum(int number)
+{
+  int sum = 0;
+  while (number > 0)
+  {
+    sum += number % 10;
+    number = number / 10;
+  }
+  return sum;
+}
+
+int num1 = ReadData("Введите число: ");
+PrintResult("Сумма цифр в числе: ", (digitSum(num1)));
