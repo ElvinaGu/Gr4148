06ef19b7714f970afefdc73fe446fca27051bcd7
Elvina <Elka121194@yandex.ru>
2023-04-09 23:19:27 +0300

    Exponentiation (A&B)

---
+++ HW.S4.Task25/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 25: 
+// Напишите цикл, который принимает на вход два числа (A и B)
+// и возводит число A в натуральную степень B.
+
+//Метод читает данные от пользователя
+int ReadData(string msg)
+{
+  Console.Write(msg);
+  int res = int.Parse(Console.ReadLine() ?? "0");
+  return res;
+}
+
+//Метод вывода результата
+void PrintResult(string msg, long res)
+{
+  Console.WriteLine(msg + res);
+}
+
+//Метод возведения числа в степень др.числа
+long Exponentiation(int numberA, int numberB)
+{
+  int res = 0;
+  res = (int)Math.Pow(numberA, numberB);
+  return res;
+}
+
+int numA = ReadData("Введите число A: ");
+int numB = ReadData("Введите число B: ");
+PrintResult("Число A в степени B: ", Exponentiation(numA, numB));
