340f35e089a67ee08a152fe5da815e0c45569e53
Elvina <Elka121194@yandex.ru>
2023-04-21 19:03:01 +0300

    The program monitors the keystrokes on the keyboard

--- HW.S6.Task41/Program.cs
+++ HW.S6.Task41/Program.cs
@@ -1,5 +1,7 @@
-﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+﻿using System.Text.RegularExpressions;
+// Задача 41: Пользователь вводит с клавиатуры M чисел. 
 // Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
 
 //Метод читает данные от пользователя
 int ReadData(string msg)
@@ -25,3 +27,47 @@ int CountPosNum(int num)
 int quanNum = ReadData("Введите количество планируемых для ввода чисел: ");
 int res = CountPosNum(quanNum);
 Console.WriteLine("Кол-во чисел больше 0: " + res);
+
+// * Пользователь вводит число нажатий, затем программа следит за нажатиями 
+// и выдает сколько чисел больше 0 было введено.
+
+int taps = ReadData("Введите количество нажатий: ");
+string inputLine = ReadLineData("Введите любые символы с клавиатуры.");
+Console.WriteLine(inputLine);
+FindNumbersInString(inputLine, taps);
+
+//метод считывает нажатие клавиш на клавиатуре заданное количество раз 
+// и собирает данные в строку через запятую
+string ReadLineData(string msg)
+{
+  Console.WriteLine(msg);
+  string line = "";
+  for (int i = 0; i < taps; i++)
+  {
+    var key = Console.ReadKey(true); //параметр true указан для того, чтобы после нажатия клавиш ничего не появлялось в консоли
+    line = line + String.Format(key.KeyChar.ToString()) + ",";
+  }
+  line = line.TrimEnd(','); //кусь последнюю запятую
+  return line;
+}
+
+// Метод для поиска десятичных цифр в строке с помощью регулярных выражений
+void FindNumbersInString(string str, int count)
+{
+  Regex regex = new Regex(@"\d");//задаем для поиска параметр "любая десятичная цифра"
+  MatchCollection matches = regex.Matches(str); //задаем коллекциюЮ состоящую из найденных по условию выражений 
+  int posNums = 0;
+  if (matches.Count > 0) //если цифры найдены, то накапливаем результат. Каждая найденная цифра +1 к итогу.
+  {
+    foreach (Match match in matches)
+    {
+      posNums = posNums + 1;
+    }
+    Console.WriteLine($"Вы совершили нажатий: {count}. Среди введенных символов найдено чисел: {posNums}");
+  }
+  else
+  {
+    Console.WriteLine($"Вы совершили нажатий: {count}. Среди введенных символов чисел не найдено");
+  }
+}
+
