635e62bc04bfb87dc1dea941a14e711c12590d12
Elvina <Elka121194@yandex.ru>
2023-04-24 19:15:14 +0300

    Pascal's Triangle

---
+++ Sem8Task61/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача №61
+// Вывести первые N строк треугольника Паскаля. Сделать вывод в виде равнобедренного треугольника.
+//             1
+//           1  1
+//         1  2  1
+//       1  3  3  1
+//     1  4  6  4  1
+//    1  5 10 10  5 1
+
+//Метод читает данные от пользователя.
+int ReadData(string msg)
+{
+  Console.Write(msg);
+  int res = int.Parse(Console.ReadLine() ?? "0");
+  return res;
+};
+
+// Метод вычисления значений Факториала
+long Factor(int n)
+{
+  long res = 1;
+  for (int i = 1; i <= n; i++)
+  {
+    res *= i;
+  }
+  return res;
+}
+
+// Метод печати Треугольника Паскаля.
+void PrintPascalTriangle(int nRaw)
+{
+  for (int i = 0; i < nRaw; i++)
+  {
+    for (int k = 0; k < nRaw - i; k++)
+    {
+      Console.Write(" ");
+    }
+    for (int j = 0; j <= i; j++)
+    {
+      Console.Write(" ");
+      Console.Write(Factor(i) / (Factor(j) * Factor(i - j)));
+    }
+    Console.WriteLine();
+  }
+}
+
+int countRaw = ReadData("Введите кол-во строк Треуголника Паскаля: ");
+PrintPascalTriangle(countRaw);
