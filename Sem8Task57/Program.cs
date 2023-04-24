e2b175a10e6e913e2ba47b5b54e688204a29f67a
Elvina <Elka121194@yandex.ru>
2023-04-24 19:13:05 +0300

    2D array. Frequency Dictionary

---
+++ Sem8Task57/Program.cs
@@ -0,0 +1,71 @@
+﻿// Задача №57
+// Составить частотный словарь элементов двумерного массива. Частотный словарь содержит
+// информацию о том, сколько раз встречается элемент входных данных.
+
+int ReadData(string msg)
+{
+  Console.Write(msg);
+  int res = int.Parse(Console.ReadLine() ?? "0");
+  return res;
+};
+
+// Метод, заполняющий двумерный массив случайными числами.
+int[,] Gen2DArray(int countRow, int countColumn, int minValue, int maxValue)
+{
+  int[,] matr = new int[countRow, countColumn];
+  for (int i = 0; i < countRow; i++)
+  {
+    for (int j = 0; j < countColumn; j++)
+    {
+      matr[i, j] = new Random().Next(minValue, maxValue + 1);
+    }
+  }
+  return matr;
+}
+
+// Метод, печатающий двумерный массив.
+void Print2DArr(int[,] matr)
+{
+
+  for (int i = 0; i < matr.GetLength(0); i++)
+  {
+    for (int j = 0; j < matr.GetLength(1); j++)
+    {
+      Console.Write($"{matr[i, j]}\t");
+    }
+    Console.WriteLine();
+  }
+}
+
+//Метод печати одномерного массива
+void Print1DArr(int[] arr)
+{
+  Console.Write("[");
+  for (int i = 0; i < arr.Length - 1; i++)
+  {
+    Console.Write(arr[i] + ",");
+  }
+  Console.WriteLine(arr[arr.Length - 1] + "]");
+}
+
+// Частотный словарь
+int[] FreqArray (int[,] arr, int count)
+{
+  int[] outArr=new int[count];
+  for(int i=0;i<arr.GetLength(0);i++)
+  {
+    for(int j=0;j<arr.GetLength(1);j++)
+    {
+      outArr[arr[i,j]]++;
+    }
+  }
+  return outArr;
+}
+
+int row = ReadData("Введите кол-во строк: ");
+int col = ReadData("Введите кол-во столбцов: ");
+int[,] arr2D = Gen2DArray(row, col, 0, 9);
+Print2DArr(arr2D);
+int[] res = FreqArray(arr2D, 10);
+Console.WriteLine();
+Print1DArr(res);
