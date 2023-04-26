d629fe856d1f3f402ad8d53a64c94c6fc32c71e0
Elvina <Elka121194@yandex.ru>
2023-04-26 22:10:47 +0300

    Recursion. Sum of numbers

---
+++ Sem9Task67/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 67
+// примает на вход число и возвращает сумму числа
+
+// Метод читает данные от пользователя.
+int ReadData(string msg)
+{
+  Console.Write(msg);
+  int res = int.Parse(Console.ReadLine() ?? "0");
+  return res;
+};
+
+// Рекурсивный метод вычисления суммы числа
+int SumDigRec(int n)
+{
+  if (n > 0)
+  {
+    return n % 10 + SumDigRec(n / 10);
+  }
+  else
+  {
+    return 0;
+  }
+}
+
+int num = ReadData("Введите число: ");
+Console.WriteLine(SumDigRec(num));
