23138c8c85044737447ae136c7cda8d631d14e4c
Elvina <Elka121194@yandex.ru>
2023-04-27 00:02:42 +0300

    Recursion. Numbers from N to 1

---
+++ HW.S9.Task64/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 64: 
+// Задайте значение N. Напишите программу, которая выведет все натуральные числа 
+// в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+//Метод читает данные от пользователя.
+int ReadData(string msg)
+{
+  Console.Write(msg);
+  int res = int.Parse(Console.ReadLine() ?? "0");
+  return res;
+};
+
+// Рекурсионный метод. Числа от N до 1.
+void LineGenRec(int numN)
+{
+  Console.Write(numN + " ");
+  if (numN <= 1)
+  {
+  }
+  else
+  {
+    LineGenRec(numN - 1);
+  }
+}
+
+int num = ReadData("Введите число N: ");
+LineGenRec(num);
