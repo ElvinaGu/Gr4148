3b8ac729fe6ff89ce6b6ca55eb5ccb470d8d1ff0
Elvina <Elka121194@yandex.ru>
2023-04-26 22:12:40 +0300

    Recursion. A to the power of B

---
+++ Sem9Task69/Program.cs
@@ -0,0 +1,41 @@
+﻿// 69 Напишите прграмму, которая будет принимать 2 числа А и В и
+// возведите А в степень В с помощью рекурсии
+
+// Метод читает данные от пользователя.
+int ReadData(string msg)
+{
+  Console.Write(msg);
+  int res = int.Parse(Console.ReadLine() ?? "0");
+  return res;
+};
+
+// Рекурсивный метод возведения числа A в степень B.
+long RecPow(int a, int b)
+{
+  if (b > 0)
+  {
+    return a * RecPow(a, b - 1);
+  }
+  else return 1;
+}
+
+long MyRecPow(int a, int b)
+{
+  if (b == 2)
+  {
+    return 4;
+  }
+  else
+  {
+    return MyRecPow(a, b / 2) * MyRecPow(a, b / 2);
+  }
+}
+
+int a = ReadData("Введите число A: ");
+int b = ReadData("Введите число B: ");
+DateTime d1 = DateTime.Now;
+Console.WriteLine(RecPow(a, b));
+Console.WriteLine(DateTime.Now - d1);
+DateTime d2 = DateTime.Now;
+Console.WriteLine(MyRecPow(a, b));
+Console.WriteLine(DateTime.Now - d2);
