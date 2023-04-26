e8cf218b32b31a54c1f0d07fee5b447918e4fc91
Elvina <Elka121194@yandex.ru>
2023-04-27 00:03:53 +0300

    Recursion. Sum of numbers from M to N

---
+++ HW.S9.Task66/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 66: 
+// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов
+// в промежутке от M до N.
+
+// Метод читает данные от пользователя.
+int ReadData(string msg)
+{
+  Console.Write(msg);
+  int res = int.Parse(Console.ReadLine() ?? "0");
+  return res;
+};
+
+// Рекурсивный метод подсчета суммы эл-ов в промежутке от M до N.
+int RecSumMN(int m, int n)
+{
+  if (m >= n) return n;
+  else return m + RecSumMN(m + 1, n);
+}
+
+int m = ReadData("Введите число M: ");
+int n = ReadData("Введите число N: ");
+
+// Защита от "дурака".
+if (m > n)
+{
+  int temp = m;
+  m = n;
+  n = temp;
+}
+
+Console.WriteLine($"Сумма эл-ов в промежутке от M до N: {RecSumMN(m, n)}");
