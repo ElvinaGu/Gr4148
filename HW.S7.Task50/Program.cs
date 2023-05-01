2435f806b4fbc1e1e847ae23c109f8649a2f1224
Elvina <Elka121194@yandex.ru>
2023-05-01 14:43:13 +0300

    2D array. The value of an element by it's index

---
+++ HW.S7.Task50/Program.cs
@@ -0,0 +1,60 @@
+﻿// Задача 50. 
+// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+// Метод читает данные от пользователя.
+int ReadData(string msg)
+{
+  Console.Write(msg);
+  int res = int.Parse(Console.ReadLine() ?? "0");
+  return res;
+};
+
+// Метод, заполняющий двумерный массив случайными числами.
+int[,] Gen2DArray(int countRow, int countColumn, int minValue, int maxValue)
+{
+  int[,] matr = new int[countRow, countColumn];
+  for (int i = 0; i < countRow; i++)
+  {
+    for (int j = 0; j < countColumn; j++)
+    {
+      matr[i, j] = new Random().Next(minValue, maxValue + 1);
+    }
+  }
+  return matr;
+}
+
+// Метод, печатающий двумерный массив.
+void Print2DArr(int[,] matr)
+{
+
+  for (int i = 0; i < matr.GetLength(0); i++)
+  {
+    for (int j = 0; j < matr.GetLength(1); j++)
+    {
+      Console.Write($"{matr[i, j]}\t");
+    }
+    Console.WriteLine();
+  }
+}
+
+// Метод поиска значения элемента по индексу.
+void FindElement(int[,] arr, int row, int column)
+{
+  if (row > arr.GetLength(0) || column > arr.GetLength(1))
+  {
+    Console.WriteLine("Такого эл-та нет");
+  }
+  else
+  {
+    Console.WriteLine("Значение эл-та: " + arr[row, column]);
+  }
+}
+
+int row = ReadData("Введите кол-во строк: ");
+int col = ReadData("Введите кол-во столбцов: ");
+int[,] arr2D = Gen2DArray(row, col, 1, 50);
+Print2DArr(arr2D);
+int rowNum = ReadData("Введите индекс строки искомого эл-та: ");
+int colNum = ReadData("Введите индекс столбца искомого эл-та: ");
+FindElement(arr2D, rowNum, colNum);
